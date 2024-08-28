0bc0a0732c203b4079ffaa078f81ad33bf45130e
Ujjwal Kumar <ujjwalkumar949494@gmail.com>
2024-08-28 14:24:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,23 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
+st.image("https://iili.io/dj5iBHB.jpg", width=700)
+# Add a small caption below the image
+st.markdown(
+    """
+    <style>
+    .caption {
+        text-align: center;
+        font-size: 12px;
+        color: white;
+        margin-top: 10px;
+    }
+    </style>
+    <div class="caption">The Team Behind This Project with our inspiring guide</div>
+    """,
+    unsafe_allow_html=True
+)
+
 # Load your trained model
 model = keras.models.load_model('./model6.h5')
 
@@ -15,7 +32,29 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 class_names = ['0.Healthy', '1.Anthracnose', '2.Phytophthora Blight', '3.Brown Spot', '4.Black Spot', '5.Others']
 
 # Streamlit UI
-st.title("Papaya Fruit Multiple Disease Classification using DL Techniques")
+st.markdown(
+    """
+    <style>
+    .title {
+        text-align: center;
+        font-size: 32px;
+        font-weight: bold;
+    }
+    .center-button {
+        display: flex;
+        justify-content: center;
+    }
+    .footer {
+        text-align: center;
+        font-size: 12px;
+        color: #888;
+        margin-top: 20px;
+    }
+    </style>
+    <div class="title">Papaya Fruit Multiple Disease Classification using DL Techniques</div>
+    """,
+    unsafe_allow_html=True
+)
 st.write("This app can predict the disease present in the papaya fruit")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
@@ -42,3 +81,13 @@ if uploaded_file is not None:
     
     # Display the prediction
     st.write(f"Predicted Disease: {predicted_class}")
+
+# Footer
+st.markdown(
+    """
+    <div class="footer">
+        Developed by <a href="https://www.linkedin.com/in/jhajibhaskar/" target="_blank">CS20B1060</a> , <a href="https://www.linkedin.com/in/abhishektirkey/" target="_blank">CS20B1002</a> , <a href="https://www.linkedin.com/in/vivek140902/" target="_blank">CS20B1065</a>, Empowering agricultural advancements with deep learning!
+    </div>
+    """,
+    unsafe_allow_html=True
+)
